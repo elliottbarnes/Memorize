1a536f506c836dcda7a16a316eaaf9b77839fc9f
elliott-barnes <elliott.barnes@verafin.com>
2021-08-11 21:11:06 -0230

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Elliott Barnes on 2021-08-11.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
